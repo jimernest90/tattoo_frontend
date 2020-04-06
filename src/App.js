9f064c661209b58464acaa456f9a0ea7b4e95e4d
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-04-05 23:39:54 -0400

    deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes/index.js'
-// import Header from './shared/Header'
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
       <Routes/>
     </div>
   );
